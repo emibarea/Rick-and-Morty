6a26128d42a4ca0b2b9498e84f78297d14ac0d03
emiliano <emilianobarea8@gmail.com>
2022-04-20 22:49:40 -0300

    cambios con css y html

--- src/template/Header.js
+++ src/template/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
         <div class="Header-logo">
             <h1>
                 <a href="/">
-                    100tifi.co
+                <img src="https://partner.redbubble.com/partnership-portal/client/eab637076b22c782edf0015a0ec3c686.png" alt="100Tifi.co">
                 </a>
             </h1>
         </div>
